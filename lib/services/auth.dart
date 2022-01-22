88b24b4d3fefe05547d009c11ef7e25487747edd
abidi <safa.abidi99@gmail.com>
2022-01-22 21:25:12 +0100

    basic app struct

---
+++ lib/services/auth.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //sign in anon
+
+  //sign in with email and pass
+
+  //register with email and pass
+
+  //sign out
+}
