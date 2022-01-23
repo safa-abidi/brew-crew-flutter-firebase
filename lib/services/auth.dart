c39038839a98ae5c7a9ded44e407ca899f933214
abidi <safa.abidi99@gmail.com>
2022-01-23 10:59:18 +0100

    sign in anon

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,10 +1,26 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //convert the firebase user into our custom one
+  User? _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid) : null ;
+  }
+
   //sign in anon
+  Future signInAnon() async {
+    try{
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    }catch(e){
+      print (e.toString());
+      return null;
+    }
+  }
 
   //sign in with email and pass
 
